7ff867ba4a71bbb316892d6986603f2f272c35c7
mrxccc <mrxccc@qq.com>
2021-06-29 16:14:25 +0800

    增加部署说明

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // const path = require('path');
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 // const contractPath = path.resolve(__dirname, '../contract_workflow/compiled/Voting.json');
-contractAddr = "0x59a4e0aba063c9C7146a0B01EBe2135340E26a1e";
+contractAddr = "0xaCCBC818BC9224A6da2902eeF82c3d14afC82aB5";
 abi = "[{\"constant\":true,\"inputs\":[],\"name\":\"getBytes32ArrayForInput\",\"outputs\":[{\"name\":\"b32Arr\",\"type\":\"bytes32[3]\"}],\"payable\":false,\"stateMutability\":\"pure\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"candiateName\",\"type\":\"bytes32\"}],\"name\":\"totalVotesFor\",\"outputs\":[{\"name\":\"\",\"type\":\"uint8\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"\",\"type\":\"bytes32\"}],\"name\":\"votesReceived\",\"outputs\":[{\"name\":\"\",\"type\":\"uint8\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"candiateListName\",\"type\":\"bytes32\"}],\"name\":\"vote\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"canditateList\",\"outputs\":[{\"name\":\"\",\"type\":\"bytes32\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"}]"
 contractInstance = new web3.eth.Contract(JSON.parse(abi),contractAddr);
 candidates = { "Candidate": "candidate-1", "Alice": "candidate-2", "Cary": "candidate-3" };
